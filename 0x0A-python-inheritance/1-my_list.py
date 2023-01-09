3c18b400f6127fe0282a2c96af612b1fb6b200f1
Wolleys <wolleysmigaya@gmail.com>
2023-01-09 10:08:51 +0000

    A class MyList that inherits from list > Public instance method: def print_sorted(self): that prints the list, but sorted (ascending sort) > You can assume that all the elements of the list will be of type int > You are not allowed to import any module

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Defines MyList class
+"""
+
+
+class MyList(list):
+    """ Class that inherits the attributes references of class list
+    Args:
+        list: class list
+    """
+
+    def print_sorted(self):
+        """ Method that prints the sorted list """
+        l_sorted = self.copy()
+        l_sorted.sort()
+        print(l_sorted)
