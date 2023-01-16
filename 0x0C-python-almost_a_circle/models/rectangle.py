f021ed612f29f3c1f9e098e7c427ae7a2d90a8c7
Wolleys <wolleysmigaya@gmail.com>
2023-01-16 12:46:53 +0000

    Update: Improve the public method def display(self) > To print in stdout the Rectangle instance with the character # by taking care of x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -52,7 +52,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """ x setter """
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
