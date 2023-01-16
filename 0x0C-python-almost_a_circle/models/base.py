a7b761749cf18c6ffd09d55e7e9252c34db3be29
Wolleys <wolleysmigaya@gmail.com>
2023-01-16 14:26:10 +0000

    Update: Static method def from_json_string(json_string)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,10 @@ class Base:
 
         with open(filename, 'w') as f:
             f.write(lists)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ JSON string to dictionary """
+        if not json_string:
+            return []
+        return json.loads(json_string)
