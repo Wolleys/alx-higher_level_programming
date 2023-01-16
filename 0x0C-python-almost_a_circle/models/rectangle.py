b68ea575f0cb847f80659dadf06fde85bcfce150
Wolleys <wolleysmigaya@gmail.com>
2023-01-16 12:21:36 +0000

    Update: Add public method def display(self)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -38,7 +38,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """ height setter """
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
