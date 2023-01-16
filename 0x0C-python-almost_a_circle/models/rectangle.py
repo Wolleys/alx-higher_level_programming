fe361d4b96a0b1b737efe84fff5f7b881f85a0d9
Wolleys <wolleysmigaya@gmail.com>
2023-01-16 15:35:36 +0000

    Update: Fix commit error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,12 +2,12 @@
 """ Module that contains class Rectangle,
 inheritance of class Base
 """
+
 from models.base import Base
 
 
 class Rectangle(Base):
     """ Class Rectangle """
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Initializes instances """
         self.width = width
