d7e8d07b4bc07bb596340c54809a64558f0b7505
Wolleys <wolleysmigaya@gmail.com>
2023-03-31 15:30:01 +0000

    Send a POST request to http://0.0.0.0:5000/search_user with a given letter

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Python script that sends a POST request to the URL and
+to an URL with the letter as a parameter
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
